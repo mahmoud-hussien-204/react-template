e0f468204a832e7f15ac34482521b1a111e433ee
Mahmoud Hussien <m.hus.dev@gmail.com>
2026-06-08 08:52:32 +0300

    commit

--- src/lib/api/interceptor.ts
+++ src/lib/api/interceptor.ts
@@ -1,1 +1,36 @@
-// s
+// import { token } from "@/modules/auth/utils/token"
+
+// const baseURL = import.meta.env.VITE_APP_API_URL
+
+export async function apiInterceptor() {
+  // url: string,
+  // options: RequestInit = { method: "GET" }
+  // try {
+  //   const request = await interceptRequest(options)
+  // } catch (error) {}
+}
+
+// async function interceptRequest(options: RequestInit) {
+//   const accessToken = token.get()
+
+//   const headers = new Headers({
+//     "Accept-Language": "en",
+//     "Content-Language": "en",
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//     ...(options.headers || {}),
+//   })
+
+//   if (accessToken) {
+//     headers.set("Authorization", `Bearer ${accessToken}`)
+//   }
+
+//   if (options.body instanceof FormData) {
+//     headers.delete("Content-Type")
+//     headers.delete("Accept")
+//   }
+
+//   options.headers = headers
+
+//   return options
+// }
